458aabd457a52f7dd0c99e0f008ddaa50bbc4549
gantasaisarvani <135020208+gantasaisarvani@users.noreply.github.com>
2024-01-08 12:20:24 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,10 +5,10 @@ st.header("Types of Birds")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("Parrot")
-  st.image("Parrot.jpg",caption="Parrot",width=300,use_column_width=True)
+  st.image("https://images.unsplash.com/photo-1552728089-57bdde30beb3?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFycm90fGVufDB8fDB8fHww",caption="Parrot",width=300,use_column_width=True)
   st.write("Parrots also known as psittacines")
 with col2:
   st.subheader("Blue bird")
-  st.image("Blue bird.jpg",caption="Blue bird",width=300,use_column_width=True)
+  st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRTvAk4ScLczGZ59IKksyOlKgLV0H1Fd102w&usqp=CAU",caption="Blue bird",width=300,use_column_width=True)
   st.write("Bluebird also known as Sialia")
   
