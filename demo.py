dc58a4267c7a6bc769d73e4435de2837a9739367
gantasaisarvani <135020208+gantasaisarvani@users.noreply.github.com>
2024-01-08 12:29:46 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config(page_title='Birds')
 st.header("Types of Birds")
 
-col1,col2=st.columns(3)
+col1,col2,col3=st.columns(3)
 with col1:
   st.subheader("Parrot")
   st.image("https://images.unsplash.com/photo-1552728089-57bdde30beb3?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFycm90fGVufDB8fDB8fHww",caption="Parrot",width=300,use_column_width=True)
