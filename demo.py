6b5bf8bc22a88636b541b910baf617a39bc5a0f4
gantasaisarvani <135020208+gantasaisarvani@users.noreply.github.com>
2024-01-08 12:35:14 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config(page_title='Birds')
 st.header("Types of Birds")
 
-col1,col2,col3=st.columns(3)
+col1,col2,col3,col4=st.columns(4)
 with col1:
   st.subheader("Parrot")
   st.image("https://images.unsplash.com/photo-1552728089-57bdde30beb3?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFycm90fGVufDB8fDB8fHww",caption="Parrot",width=300,use_column_width=True)
@@ -14,6 +14,10 @@ with col2:
 with col3:
   st.subheader("Bird Singing")
   st.image("https://media3.giphy.com/media/KfxPezYC20OnSPQK5J/giphy.gif",caption="Blue bird",width=300,use_column_width=True)
-  st.write("Birdsn are so beautiful")
+  st.write("Birds are so beautiful")
+with col4:
+  st.subheader("Blue bird moving")
+  st.image("https://peter-ertl.com/birds/images/blaumeise-5.gif",caption="Blue bird",width=300,use_column_width=True)
+  st.write("Blue Birds are so beautiful")
 
 
