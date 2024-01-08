258db2ef5526c32044103af1c4c2d1626219bf91
gantasaisarvani <135020208+gantasaisarvani@users.noreply.github.com>
2024-01-08 11:51:33 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.set_page_config(page_title='Cats')
+st.header("Types of Cats")
+
+col1,col2=st.columns(2)
+with col1:
+  st.subheader("Parrot")
+  st.image("./Parrot.jpg",caption="Parrot",width=300,use_column_width=True)
+  st.write("Parrots also known as psittacines")
+with col2:
+  st.subheader("Blue bird")
+  st.image("./Blue bird.jpg",caption="Blue bird",width=300,use_column_width=True)
+  st.write("Bluebird also known as Sialia")
+  
